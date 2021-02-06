4e72f3bb1e791682e8b6174dc17c9289e84a02da
Ivan Chan <iot3534199@gmail.com>
2021-02-07 08:58:37 +1100

    Added button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "doc appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "doc appointment2",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "doc appointment3",
+      day: "Feb 5th at 2:30pm",
+      reminder: false,
+    },
+  ]);
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
